976d11abb26dab3166db9bd3b87df6a42b20bcad
jash-git <jash.liao@gmail.com>
2018-11-20 15:04:45 +0800

    Create

---
+++ 原始網頁/【心理測驗】你第一眼看到什麼動物？別懷疑，這能測出你的「潛在性格」！ _ VidaOrange_files/cookie_transfer.jsp
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <html>
+    <head>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta name="robots" content="noindex, nofollow, noarchive, none">
+    </head>
+        <body>
+            
+            <img src="//m.doublemax.net/ml/eland?vid=20787643bf67420ba57b08d97b5fe0e5110512" style="display: none"/>
+            
+        </body>
+    </html>
+
---
+++ 原始網頁/第一眼你看到什麼動物呢！答案透露出「你的真實性格」！ - COCO01_files/external.php
@@ -0,0 +1,35 @@
+/* in-freq, hit 2 tw */
+if (ONEAD_get_response[1] !== undefined && typeof ONEAD_get_response[1] == 'function'){
+    ONEAD_get_response[1]({
+      "guid": "0dfd8a97-e0af-11e8-9aed-0242ac120003"
+    , "pid": 1167462
+    , "uid": 1000107
+    , "adv_id": 30
+    , "cm_custom_id": ""
+    , "camp_id": 10762
+    , "ct_id": 2
+    , "pm_id": 8826
+    , "cm": "false"
+    , "play_mode": "inread"
+    , "run": "2:324336-320775 [400 401 402 404 407 410 513 551 640 650 840 850 860 870 930 932 941 943 951 953 961 971]"
+    , "tout": 3000
+    , "is_cprp": "true"
+    , "is_vast": "false"
+    , "is_custom_pid": "false"
+    , "force_backend": "false"
+    , "term_id":  "-1" 
+    , "player_type": "HTML5_IS_DF"
+    , "isip_index": 1
+    , "width": 640
+    , "height": 390
+    , "stage_width": 740
+    , "stage_height": 450
+    , "base_url": "https://onead.onevision.com.tw/"
+    , "iframe_url": ""
+    , "tracking": "//onead.onevision.com.tw/v2/async?_hash=8678482772004443345&action=&c1=1167462&c2=inread&clicklabel=[CLICKLABEL]&cmpid=10762&ctid=2&dt=1542697239&errorcode=[ERRORCODE]&errordesc=[ERRORDESC]&errormsg=[ERRORMSG]&event=[EVENT]&gc=2&host=https%3A%2F%2Fonead.onevision.com.tw%2F&ix=1&p1=1000107&p10=101%2C105%2C108%2C111%2C112%2C309%2C312%2C313%2C404%2C418%2C420%2C601%2C602%2C610%2C612%2C702%2C705%2C709%2C717%2C902%2C903%2C904%2C913%2C914&p11=24&p12=3%2C5&p13=13%2C3%2C12%2C2%2C8%2C11%2C4%2C9%2C6&p14=&p15=&p2=0dfd8a97-e0af-11e8-9aed-0242ac120003&p3=http%3A%2F%2Fwww.coco01.today%2Fpost%2F608912&p4=%E7%AC%AC%E4%B8%80%E7%9C%BC%E4%BD%A0%E7%9C%8B%E5%88%B0%E4%BB%80%E9%BA%BC%E5%8B%95%E7%89%A9%E5%91%A2%EF%BC%81%E7%AD%94%E6%A1%88%E9%80%8F%E9%9C%B2%E5%87%BA%E3%80%8C%E4%BD%A0%E7%9A%84%E7%9C%9F%E5%AF%A6%E6%80%A7%E6%A0%BC%E3%80%8D%EF%BC%81+-+COCO01&p5=&p6=336115690e76a8fafb5987b1914b29ff%3A180.217.75.153&p7=-1&p8=612%2C602%2C601%2C610%2C418%2C101%2C420%2C702&p9=110%2C1110%2C1012%2C15%2C811%2C1101%2C1109%2C615%2C9%2C7%2C714%2C609%2C13%2C103%2C1422%2C817%2C201%2C1310%2C701%2C1001%2C406%2C1002%2C1107%2C612%2C604%2C602%2C3%2C607%2C605%2C1203%2C413%2C619%2C1419%2C1111%2C616%2C14%2C1421%2C315%2C713%2C12%2C614%2C613%2C611%2C601%2C804%2C1301%2C818%2C1102%2C813%2C716%2C816%2C1%2C621%2C1113%2C5%2C8%2C809%2C618%2C11%2C4%2C510%2C803%2C1204%2C711%2C1007%2C610%2C504%2C501%2C106%2C113%2C608%2C1418%2C810%2C418%2C101%2C1507%2C805%2C1308%2C1013%2C420%2C107%2C1016%2C203%2C2%2C109%2C10%2C806%2C210%2C702%2C815%2C617%2C603%2C1420%2C1202%2C620%2C211%2C1009%2C808%2C6%2C1011&plaid=8826&pms=inread&vol=0.2"
+    , "l_url": "https://onead.onevision.com.tw/cprp/xml/1167462/l.xml?pid=1167462&campid=10762&uid=1000107&guid=0dfd8a97-e0af-11e8-9aed-0242ac120003"
+    });
+
+}
+/* [server]/client */
+
